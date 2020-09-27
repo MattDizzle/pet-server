920efe074f175c773725ab0497dffa53c39b8927
Matthew Rougely <matthewrougelysr@gmail.com>
2020-09-26 22:20:59 -0700

    .

--- src/modules/app/app.js
+++ src/modules/app/app.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 const { CLIENT_ORIGIN } = require('../../config')
 const app = express()
 
-// app.use(
-// 	cors({
-// 		origin: CLIENT_ORIGIN,
-// 	})
-// )
+app.use(
+	cors({
+		origin: 'https://pet-client.vercel.app',
+	})
+)
 
 app.use('/api/people', require('../people/people.router'))
 app.use('/api/pets', require('../pets/pets.router'))
